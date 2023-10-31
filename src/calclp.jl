16f8331f8a65d968d5d4c6c3640c01c294ad5d06
abikoushi <a.b.e.k.o.69@gmail.com>
2023-10-31 15:37:36 +0900

    fix

--- src/calclp.jl
+++ src/calclp.jl
@@ -22,26 +22,26 @@ function evaluatelp(d::UnivariateDistribution, x::DIC)
     return ll
 end
 
-function evaluatelp(d::UnivariateDistribution, x::DICRT)
-    ll = zero(x.EL)
-    mu = mean(d)
-    if isfinite(x.EL)
-        if x.ER < x.SL
-            ll += log(eqcdf(d, x.SR-x.ER) - eqcdf(d, x.SL-x.ER) - (eqcdf(d, x.SR-x.EL) - eqcdf(d, x.SL-x.EL))) - (log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL)))
-        elseif x.SL < x.ER < x.SR
-            ll += log((x.ER-x.SL) + mu*(eqcdf(d, x.SR-x.ER) - (eqcdf(d, x.SR-x.EL) - eqcdf(d, x.SL-x.EL)))) - (log(mu) + log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL)))
-        elseif x.SR <= x.ER
-            ll += log( (x.SR - x.SL) - mu*(eqcdf(d, x.SR-x.EL) - eqcdf(d, x.SL-x.EL)) ) - (log(mu) + log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL)))
-        end
-    else #EL is missing
-        if x.ER < x.SL
-            ll += log(eqcdf(d, x.SR-x.ER)-eqcdf(d, x.SL-x.ER)) - (logmu+log(eqcdf(d,x.TR-x.ER))) #- log(x.SR - x.SL)
-        elseif x.ER <= x.SR
-            ll += log(eqcdf(d, x.SR-x.ER)) - (logmu+log(eqcdf(d,x.TR-x.ER))) 
-        end
-    end
-    return ll
-end
+# function evaluatelp(d::UnivariateDistribution, x::DICRT)
+#     ll = zero(x.EL)
+#     mu = mean(d)
+#     if isfinite(x.EL)
+#         if x.ER < x.SL
+#             ll += log(eqcdf(d, x.SR-x.ER) - eqcdf(d, x.SL-x.ER) - (eqcdf(d, x.SR-x.EL) - eqcdf(d, x.SL-x.EL))) - (log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL)))
+#         elseif x.SL < x.ER < x.SR
+#             ll += log((x.ER-x.SL) + mu*(eqcdf(d, x.SR-x.ER) - (eqcdf(d, x.SR-x.EL) - eqcdf(d, x.SL-x.EL)))) - (log(mu) + log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL)))
+#         elseif x.SR <= x.ER
+#             ll += log( (x.SR - x.SL) - mu*(eqcdf(d, x.SR-x.EL) - eqcdf(d, x.SL-x.EL)) ) - (log(mu) + log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL)))
+#         end
+#     else #EL is missing
+#         if x.ER < x.SL
+#             ll += log(eqcdf(d, x.SR-x.ER)-eqcdf(d, x.SL-x.ER)) - (logmu+log(eqcdf(d,x.TR-x.ER))) #- log(x.SR - x.SL)
+#         elseif x.ER <= x.SR
+#             ll += log(eqcdf(d, x.SR-x.ER)) - (logmu+log(eqcdf(d,x.TR-x.ER))) 
+#         end
+#     end
+#     return ll
+# end
 
 #interval censored
 function evaluatelp(d::UnivariateDistribution, x::IC)
@@ -55,16 +55,16 @@ function evaluatelp(d::UnivariateDistribution, x::IC)
     return ll
 end
 
-function evaluatelp(d::UnivariateDistribution, x::ICRT)
-    ll = zero(x.EL)
-    logmu = logmean(d)
-    if isfinite(x.EL)
-        ll += log(cdf(d, x.S-x.EL) - cdf(d, x.S-x.ER)) - (logmu+log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL))) #- log(x.ER - x.EL)
-    else
-        ll += log(ccdf(d, x.S-x.ER)) - logmu - log(eqcdf(d,x.TR-x.ER))
-    end
-    return ll
-end
+# function evaluatelp(d::UnivariateDistribution, x::ICRT)
+#     ll = zero(x.EL)
+#     logmu = logmean(d)
+#     if isfinite(x.EL)
+#         ll += log(cdf(d, x.S-x.EL) - cdf(d, x.S-x.ER)) - (logmu+log(eqcdf(d,x.TR-x.ER)-eqcdf(d,x.TR-x.EL))) #- log(x.ER - x.EL)
+#     else
+#         ll += log(ccdf(d, x.S-x.ER)) - logmu - log(eqcdf(d,x.TR-x.ER))
+#     end
+#     return ll
+# end
 
 function evaluatelp(d::UnivariateDistribution, x::NC)
     return logpdf(d, x.S-x.E)
